5b3e0b7ca1c535a42e07898d67efa64cd32587a9
zhangbin <zhangbin@uni-ubi.com>
2019-03-25 15:39:50 +0800

    maven仓库

--- src/main/java/com/zhangbin/api/TestController.java
+++ src/main/java/com/zhangbin/api/TestController.java
@@ -1,11 +1,16 @@
 package com.zhangbin.api;
 
 import com.zhangbin.dto.CarDto;
+import com.zhangbin.enums.TypeEnum;
 import com.zhangbin.vo.CarVO;
 import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @RestController
 public class TestController {
@@ -17,6 +22,18 @@ public class TestController {
         BeanUtils.copyProperties(carDto, carVO);
         carVO.setId(carVO.getId() + 1);
         carVO.setType(carDto.getPayType());
+        return carVO;
+    }
+
+    @GetMapping("/test2")
+    public CarVO test() {
+        CarVO carVO = new CarVO();
+        carVO.setId(1L);
+        carVO.setCreatedAt(LocalDateTime.now());
+        carVO.setDate(new Date());
+        carVO.setType(TypeEnum.BIG);
+        carVO.setName("zhangsan");
+
         return carVO;
     }
 
